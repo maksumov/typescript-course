7a57bde72c55592f598e95345a82b6708c72deae
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-05 17:49:57 +0300

    chore: commit before refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort() {
     const { length } = this.collection;
 
-    for (let i = 0; i < length - 1; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const a = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = a;
+    // All this things works only if collection is number[]
+    // (if collection is an array of numbers)
+    if (this.collection instanceof Array) {
+      // <-- This is type guard
+      for (let i = 0; i < length - 1; i++) {
+        for (let j = 0; j < length - i - 1; j++) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const a = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = a;
+          }
         }
       }
+    }
+
+    // Only going to work if collection is a string
+    // If collection is a string do this logic instead:
+    // ~~~~ logic to compare and swap characters in a string
+    // Issues: 1. 'X' < 'a'
+    //         2. collection[i] is readonly operation
+    if (typeof this.collection === "string") {
+      //
     }
   }
 }
