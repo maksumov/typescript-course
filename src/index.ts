acad20d4fc889531a2295b23708d70bdd445cba4
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-06 21:22:24 +0300

    feat: add LinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { CharactersCollection } from "./models/CharactersCollection";
+// import { CharactersCollection } from "./models/CharactersCollection";
+import { LinkedList } from "./models/LinkedList";
 // import { NumbersCollection } from "./models/NumbersCollection";
 import { Sorter } from "./Sorter";
 
@@ -8,8 +9,20 @@ import { Sorter } from "./Sorter";
 // sorter.sort();
 // console.log("numbersCollection.data", numbersCollection.data);
 
-const charCollection = new CharactersCollection("CharactersCollection");
-const sorter2 = new Sorter(charCollection);
-console.log("charCollection", charCollection.data);
-sorter2.sort();
-console.log("charCollection", charCollection.data);
+// const charCollection = new CharactersCollection("CharactersCollection");
+// const sorter2 = new Sorter(charCollection);
+// console.log("charCollection", charCollection.data);
+// sorter2.sort();
+// console.log("charCollection", charCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-4);
+linkedList.add(12);
+
+linkedList.print();
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
