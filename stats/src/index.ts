0d8dbb941f884d41b33fb234f324bc58e707929d
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-07 06:34:26 +0300

    feat: add reading from csv file

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,3 +1,7 @@
 import fs from "fs";
 
-const data = fs.readFileSync("../football.csv");
+const matches = fs
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((match: string): string[] => match.split(","));
+console.log("match results", matches);
