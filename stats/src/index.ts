3a70041c0e47c69c0c5ad13a82da9cd81a722596
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-07 15:27:18 +0300

    feat: add Man United games win calculator

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -4,4 +4,15 @@ const matches = fs
   .readFileSync("football.csv", { encoding: "utf-8" })
   .split("\n")
   .map((match: string): string[] => match.split(","));
-console.log("match results", matches);
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === "H") {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === "A") {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
