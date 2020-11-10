dfb327033a7a27a76a7d703b376ad2d45c1db2d6
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-10 09:27:53 +0300

    refactor: refactor with object definition

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,12 +5,18 @@ const matches = fs
   .split("\n")
   .map((match: string): string[] => match.split(","));
 
+const MatchResult = {
+  HomeWin: "H",
+  AwayWin: "A",
+  Draw: "D",
+};
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
