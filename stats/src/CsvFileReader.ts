04e909596931fa93ecd3a99f41ce1edc75ec679e
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-10 12:18:33 +0300

    feat: add type conversion in csv reader
    extract MatchResult
    add conversion from dateString to DateString
    add type conversion for CSV row elements

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,4 +1,6 @@
 import fs from "fs";
+import { MatchResult } from "./MatchResult";
+import { dateStringToDate } from "./utils";
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -9,6 +11,17 @@ export class CsvFileReader {
     this.data = fs
       .readFileSync(this.filename, { encoding: "utf-8" })
       .split("\n")
-      .map((match: string): string[] => match.split(","));
+      .map((row: string): string[] => row.split(","))
+      .map((row: string[]): any => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult, // Type assertion
+          row[6],
+        ];
+      });
   }
 }
