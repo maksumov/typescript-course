ea7f8e7e2e1532267a12d20066acfd39ee88e6ff
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-10 13:05:25 +0300

    refactor: make CsvFileReader an abstract class

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,13 +1,13 @@
 import fs from "fs";
-import { MatchResult } from "./MatchResult";
-import { dateStringToDate } from "./utils";
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export class CsvFileReader {
+export abstract class CsvFileReader {
   data: MatchData[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): MatchData;
 
   read() {
     this.data = fs
@@ -15,17 +15,5 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => row.split(","))
       .map(this.mapRow);
-  }
-
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult, // Type assertion
-      row[6],
-    ];
   }
 }
