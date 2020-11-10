abef20a32858e66dd0fd9a17f0386ce9fc4cdbf8
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-10 14:28:59 +0300

    refactor: finish alternate refactor (interfaces)

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,11 +1,7 @@
 import fs from "fs";
-import { MatchResult } from "./MatchResult";
-import { dateStringToDate } from "./utils";
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export class CsvFileReader {
-  data: MatchData[] = [];
+  data: string[][] = [];
 
   constructor(public filename: string) {}
 
@@ -13,19 +9,6 @@ export class CsvFileReader {
     this.data = fs
       .readFileSync(this.filename, { encoding: "utf-8" })
       .split("\n")
-      .map((row: string): string[] => row.split(","))
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult, // Type assertion
-            row[6],
-          ];
-        }
-      );
+      .map((row: string): string[] => row.split(","));
   }
 }
