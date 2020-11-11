e08bcf4cfd037f088ec8efad28a48b45ddda508d
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-12 02:13:15 +0300

    feat: reassemble with Summary

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,8 @@
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { Summary } from "./Summary";
 
 // Create an object that satisfies the DataReader interface
 const csvFileReader = new CsvFileReader("football.csv");
@@ -10,14 +12,5 @@ const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(new WinsAnalysis("Man City"), new ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
