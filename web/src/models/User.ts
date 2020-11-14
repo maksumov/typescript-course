a40b8438a3ecd7313d9860d0830c0678b9901822
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-14 07:54:00 +0300

    refactor: reintegrate Attributes back into User

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,3 +1,4 @@
+import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 
@@ -12,4 +13,9 @@ const rootUrl = "http://localhost:3000/users";
 export class User {
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
   public events: Eventing = new Eventing();
+  public attributes: Attributes<UserProps>;
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
 }
