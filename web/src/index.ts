650836affdbe5ec8bf8f0b188dea95cd83455b14
Marat Maksumov <maksumov@users.noreply.github.com>
2020-11-14 03:17:05 +0300

    feat: add fetch method
    implement fetch data from REST API (db)
    add code for test fetch functionality

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,8 @@
-import axios from "axios";
+import { User } from "./models/User";
 
-axios.post("http://localhost:3000/users", {
-  name: "Petya",
-  age: 27,
-});
+const user = new User({ id: 1 });
+user.fetch();
+
+setTimeout(() => {
+  console.log(user);
+}, 4000);
