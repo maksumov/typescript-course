19a8b7b91172541e381ef2c49fcd32c1b109219c
Marat Maksumov <maksumov@users.noreply.github.com>
2021-01-24 02:10:09 +0300

    refactor(index.ts): delete unused code

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,10 +1,6 @@
 import { User } from './models/User';
 
-// const user = new User({ id: 1 });
-// user.set({ name: "Non-Vasya", age: 9999 });
 const user = new User({ id: 1 });
-
-console.log(user.get('name'));
 
 user.on('change', () => {
   console.log(user);
