76a7666d62c8e48fe5edfa553002debf60ce1617
Marat Maksumov <maksumov@users.noreply.github.com>
2021-01-24 11:52:41 +0300

    feat: extract Model & extend User

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,8 +1,4 @@
-import { AxiosResponse } from 'axios';
-import { Attributes } from './Attributes';
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
-
+import { Model } from './Model';
 export interface UserProps {
   id?: number;
   name?: string;
@@ -11,55 +7,4 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public events: Eventing = new Eventing();
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  get get() {
-    // return this.attributes.get.bind(this.attributes);
-    return this.attributes.get;
-  }
-
-  get on() {
-    // return this.events.on.bind(this.events);
-    return this.events.on;
-  }
-
-  get trigger() {
-    // return this.events.trigger.bind(this.events);
-    return this.events.trigger;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id');
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch data withoud an id');
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.events.trigger('save');
-      })
-      .catch(() => {
-        this.trigger('error');
-      });
-  }
-}
+export class User extends Model<UserProps> {}
