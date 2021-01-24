d8815011eda24a0f1bf64e471bdde8ad99f29a09
Marat Maksumov <maksumov@users.noreply.github.com>
2021-01-24 19:06:22 +0300

    feat: implement event handlers binding

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -4,11 +4,16 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover,
     };
   }
 
+  onHeaderHover(): void {
+    console.log('H1 was hovered over');
+  }
+
   onButtonClick(): void {
-    console.log('Button clicked');
+    console.log('Button was clicked');
   }
 
   template(): string {
@@ -16,13 +21,28 @@ export class UserForm {
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Click Me</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
