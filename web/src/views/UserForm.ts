b3f6f1ed451655550d19dc5543e6cd9d887c9292
Marat Maksumov <maxumov@gmail.com>
2021-01-24 23:06:16 +0300

    feat: bind events on class name

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -4,17 +4,12 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeaderHover,
+      'click:.set-age': this.onSetAgeClick,
     };
   }
 
-  onHeaderHover(): void {
-    console.log('H1 was hovered over');
-  }
-
-  onButtonClick(): void {
-    console.log('Button was clicked');
+  onSetAgeClick(): void {
+    console.log('Set Age Button was clicked');
   }
 
   template(): string {
@@ -25,6 +20,7 @@ export class UserForm {
         <div>User age: ${this.model.get('age')}</div>
         <input />
         <button>Click Me</button>
+        <button class="set-age">Set Random Age</button>
       </div>
     `;
   }
