88d35c45276fac98fbbc0da9bd1a54605e2df77c
Marat Maksumov <maxumov@gmail.com>
2021-01-25 23:27:28 +0300

    feat: extend Request type definition (basic way)

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Request, Response, Router } from 'express';
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
@@ -18,10 +22,14 @@ router.get('/login', (req: Request, res: Response) => {
   `);
 });
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  res.send({ email, password });
+  if (email) {
+    res.send(email.toUpperCase());
+  } else {
+    res.send('You must provide an email');
+  }
 });
 
 export { router };
