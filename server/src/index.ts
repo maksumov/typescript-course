063127e8774fee339415facaa4748578db7f01ca
Marat Maksumov <maxumov@gmail.com>
2021-01-25 20:18:26 +0300

    feat: add login form & form bodies parsing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import { router } from './routes/loginRoutes';
 
 const PORT = 3000;
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(PORT, () => {
