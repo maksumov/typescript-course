65d5313949cad172466db1e5cd901741608a64f5
Marat Maksumov <maksumov@users.noreply.github.com>
2021-01-25 03:47:33 +0300

    feat: add mapping regions

--- web/src/views/UserEdit.ts
+++ web/src/views/UserEdit.ts
@@ -2,11 +2,18 @@ import { User, UserProps } from '../models/User';
 import { View } from './View';
 
 export class UserEdit extends View<User, UserProps> {
+  regionsMap(): { [key: string]: string } {
+    return {
+      userShow: '.user-show',
+      userForm: '.user-form',
+    };
+  }
+
   template() {
     return `
       <div>
         <div class="user-show"></div>
-        <div class="user-edit"></div>
+        <div class="user-form"></div>
       </div>
     `;
   }
