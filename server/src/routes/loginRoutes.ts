e573353f5c36ca513c258d6f4e5bc75f25e087ac
Marat Maksumov <maksumov@users.noreply.github.com>
2021-01-26 01:05:34 +0300

    feat: add protected route & route protection middleware

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,16 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session?.loggedIn === true) {
+    next();
+    return;
+  }
+
+  res.status(403).send('Not permitted');
 }
 
 const router = Router();
@@ -54,6 +63,10 @@ router.get('/', (req: Request, res: Response) => {
 router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user!');
 });
 
 export { router };
