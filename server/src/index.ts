f008101f0896648932ee5dfd8534296101b7a31c
Marat Maksumov <maxumov@gmail.com>
2021-01-25 19:49:45 +0300

    feat: basic express server up & running
    1. add the following dependencies:
    - express
    - body-parser
    - cookie-session
    - @types/express
    - @types/body-parser
    - @types/cookie-session
    2. add basic express boilerplate

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hi there');
+import express, { Request, Response } from 'express';
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+  <div>
+    <h1>Hi there!</h1>
+  </div>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
