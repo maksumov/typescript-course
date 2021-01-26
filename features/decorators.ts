00764ae6ec8871912d0d1de232631353e8f72169
Marat Maksumov <maksumov@users.noreply.github.com>
2021-01-26 11:25:45 +0300

    feat: add decorator factory

--- features/decorators.ts
+++ features/decorators.ts
@@ -5,22 +5,24 @@ class Boat {
     return `This boats color is ${this.color}`;
   }
 
-  @logError
+  @logError('Oops... boat was sunk in ocean')
   pilot(): void {
     throw new Error();
     console.log('swish');
   }
 }
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  const method = desc.value;
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
 
-  desc.value = function () {
-    try {
-      method();
-    } catch (e) {
-      console.log('Oops, boat was sunk');
-    }
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
   };
 }
 
