b842adf9cef76bee20e776e08c56ebac1c567e14
Marat Maksumov <maxumov@gmail.com>
2021-02-02 14:53:04 +0300

    feat: add get decorator

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -42,24 +42,6 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   }
 });
 
-router.get('/', (req: Request, res: Response) => {
-  if (req.session?.loggedIn === true) {
-    res.send(`
-      <div>
-        <div>You are logged in</div>
-        <a href="/logout">Logout</a>
-      </div>
-    `);
-  } else {
-    res.send(`
-      <div>
-        <div>You are not logged in</div>
-        <a href="/login">Login</a>
-      </div>
-    `);
-  }
-});
-
 router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect('/');
