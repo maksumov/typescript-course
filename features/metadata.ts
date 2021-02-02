97f0a61fa2cdeaf2bc6033331fb02cff9cd311e2
Marat Maksumov <maksumov@users.noreply.github.com>
2021-02-02 08:19:39 +0300

    feat: add basic demo of metadata

---
+++ features/metadata.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+
+const plane = {
+  color: 'red',
+};
+
+Reflect.defineMetadata('note', 'hi there', plane);
+
+console.log('plane', plane);
+
+const note = Reflect.getMetadata('note', plane);
+
+console.log('note', note);
