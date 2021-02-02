f1d69174791455be10864f31daf49c19d059cad1
Marat Maksumov <maksumov@users.noreply.github.com>
2021-02-02 09:30:28 +0300

    feat: add class decorator to previous example

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 
+@printMetadata
 class Plane {
   color: string = 'red';
 
@@ -15,6 +16,9 @@ function markFunction(secretInfo: string) {
   };
 }
 
-const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
-
-console.log(secret);
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
