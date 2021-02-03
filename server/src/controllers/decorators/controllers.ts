c30111a720745ea69a1277b6ba2aacd747339f62
Marat Maksumov <maksumov@users.noreply.github.com>
2021-02-04 00:46:58 +0300

    feat: add AppRouter singleton

--- server/src/controllers/decorators/controllers.ts
+++ server/src/controllers/decorators/controllers.ts
@@ -1,10 +1,10 @@
-import express from 'express';
 import 'reflect-metadata';
-
-export const router = express.Router();
+import { AppRouter } from '../../AppRouter';
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
+    const router = AppRouter.getInstance();
+
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata('path', target.prototype, key);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
-import { router as controllerRouter } from './controllers/decorators/controllers';
+import { AppRouter } from './AppRouter';
 import './controllers/loginController';
 import { router } from './routes/loginRoutes';
 
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['kjasdasd'] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
