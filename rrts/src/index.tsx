8221cd4d7e19bc9a743b37997747ddf1acfc8421
Marat Maksumov <maxumov@gmail.com>
2021-02-05 19:29:49 +0300

    Revert "feat: add example of class component state interface"
    This reverts commit e667e0b4ae04a083c56b5dff4b6458211003174a.
    This is used to sync with course content but save functional component
    example in git history...

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,9 +1,30 @@
+import { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 interface AppProps {
   color?: string;
 }
 
-const App = (props: AppProps): JSX.Element => <div>{props.color}</div>;
+class App extends Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App color="red" />, document.querySelector('#root'));
