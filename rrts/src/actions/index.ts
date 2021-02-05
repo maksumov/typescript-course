b525d8d6aeb590d4ceeb7c857d2fed9506be987a
Marat Maksumov <maxumov@gmail.com>
2021-02-05 20:18:13 +0300

    feat: add Todo interface and use it for getting axios data response

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -1,11 +1,17 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
+    const response = await axios.get<Todo[]>(url);
 
     dispatch({
       type: 'FETCH_TODOS',
