ca3c45e50e97e0b673704cab1e1b1a45d9f2b702
Marat Maksumov <maxumov@gmail.com>
2021-02-05 21:16:26 +0300

    feat: connect App component to redux

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchTodos, Todo } from '../actions/index';
+import { StoreState } from '../reducers';
 
-export class App extends Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+export class _App extends Component<AppProps> {
   render() {
     return <div>Hi there</div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
