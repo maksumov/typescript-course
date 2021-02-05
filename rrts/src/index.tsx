404247c9f187a32945adcc8d06087dce8a5ed3c1
Marat Maksumov <maxumov@gmail.com>
2021-02-05 19:19:40 +0300

    feat: add example of functional component

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,38 +1,9 @@
-import { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 interface AppProps {
   color?: string;
 }
 
-interface AppState {
-  counter: number;
-}
+const App = (props: AppProps): JSX.Element => <div>{props.color}</div>;
 
-class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-
-    this.state = { counter: 0 };
-  }
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
