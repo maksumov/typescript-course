6ca9858cf96e7b00c9474a19e99e3fccab3784d1
Marat Maksumov <maksumov@users.noreply.github.com>
2021-02-06 01:20:56 +0300

    feat: add Action as a union of action types

--- rrts/src/reducers/todos.ts
+++ rrts/src/reducers/todos.ts
@@ -1,6 +1,6 @@
-import { ActionTypes, FetchTodosAction, Todo } from '../actions';
+import { Action, ActionTypes, Todo } from '../actions';
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
