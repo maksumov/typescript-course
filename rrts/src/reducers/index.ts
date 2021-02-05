ce8ffa275af6acb7380060d88b6965ffba56df1d
Marat Maksumov <maxumov@gmail.com>
2021-02-05 20:58:26 +0300

    feat: update Store structure and make it type-safe

--- rrts/src/reducers/index.ts
+++ rrts/src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
+import { Todo } from '../actions/index';
+import { todosReducer } from './todos';
 
-export const reducers = combineReducers({
-  counter: () => 1,
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer,
 });
