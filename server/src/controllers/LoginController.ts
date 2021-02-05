9e01d3fd8a87cfc110de31e79c574987551008af
Marat Maksumov <maxumov@gmail.com>
2021-02-05 15:18:44 +0300

    feat: rewrite all routes with decorators-based approach
    Some changes:
    rename loginController.ts to LoginController.ts
    move /logout route to LoginController
    move root routes to src/controllers/RootController.ts
    remove src/routes/loginRoutes.ts

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -34,4 +34,10 @@ class LoginController {
       res.send('Invalid email or password');
     }
   }
+
+  @get('/logout')
+  getLogout(req: Request, res: Response): void {
+    req.session = undefined;
+    res.redirect('/');
+  }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,15 +2,14 @@ import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 import { AppRouter } from './AppRouter';
-import './controllers/loginController';
-import { router } from './routes/loginRoutes';
+import './controllers/LoginController';
+import './controllers/RootController';
 
 const PORT = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['kjasdasd'] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(PORT, () => {
